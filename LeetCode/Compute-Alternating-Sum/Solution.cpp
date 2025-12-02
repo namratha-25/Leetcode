e46476638d1a2eeedf7cf538d609f10545536abf
namratha-25 <vankanamratha06@gmail.com>
2025-12-02 11:34:45 +0530

    Add solution for Compute Alternating Sum

--- LeetCode/Compute-Alternating-Sum/Solution.cpp
+++ LeetCode/Compute-Alternating-Sum/Solution.cpp
@@ -1,14 +1,14 @@
 1class Solution {
 2public:
 3    int alternatingSum(vector<int>& nums) {
-4        int se = 0, so = 0;
+4        int se = 0;
 5        for(int i = 0;i<nums.size();i++)
 6        {
 7            if(i%2==0)
 8            se+=nums[i];
 9            else
-10            so+=nums[i];
+10            se-=nums[i];
 11        }
-12        return se-so;
+12        return se;
 13    }
 14};

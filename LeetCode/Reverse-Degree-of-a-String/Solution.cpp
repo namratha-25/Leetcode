c3d4a509d96220fb640755004d121f2005a36e41
namratha-25 <vankanamratha06@gmail.com>
2025-12-02 11:46:09 +0530

    Add solution for Reverse Degree of a String

--- LeetCode/Reverse-Degree-of-a-String/Solution.cpp
+++ LeetCode/Reverse-Degree-of-a-String/Solution.cpp
@@ -4,7 +4,7 @@
 4        int sum = 0;
 5        for(int i = 0;i<s.size();i++)
 6        {
-7            sum+=(27-(int(s[i])-96))*(i+1);
+7            sum+=(26-(s[i]-'a'))*(i+1);
 8        }
 9        return sum;
 10    }
